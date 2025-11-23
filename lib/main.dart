168d53e92bbb518eb22ef635ed80bd8bbcb4122b
RonydaEssam <ronyda988@gmail.com>
2025-11-23 14:50:59 +0200

    setting up theme and main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,28 @@
+import 'package:favourite_places/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final colorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 102, 6, 247),
+  surface: Color.fromARGB(255, 56, 49, 66),
+);
+
+final theme = ThemeData().copyWith(
+  scaffoldBackgroundColor: colorScheme.surface,
+  colorScheme: colorScheme,
+  textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+    titleSmall: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleMedium: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+    titleLarge: GoogleFonts.ubuntuCondensed(
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+);
 
 void main() {
   runApp(MyApp());
@@ -11,7 +35,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      title: 'Favourite Places',
+      theme: theme,
+      home: MainScreen(),
     );
   }
 }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Favourite Places'),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+      ),
+      body: ListView.builder(
+        itemBuilder: (context, index) {},
+      ),
+    );
+  }
+}
