1153b33dc9a449040d6b40d72994639128e7e3b8
RonydaEssam <ronyda988@gmail.com>
2025-12-11 11:59:56 +0200

    use the local database to get stored data if exists

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -4,11 +4,26 @@ import 'package:favourite_places/screens/add_place_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MainScreen extends ConsumerWidget {
+class MainScreen extends ConsumerStatefulWidget {
   const MainScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<MainScreen> createState() {
+    return _MainScreenState();
+  }
+}
+
+class _MainScreenState extends ConsumerState<MainScreen> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(userPlacesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final userPlaces = ref.watch(userPlacesProvider);
 
     return Scaffold(
@@ -27,7 +42,13 @@ class MainScreen extends ConsumerWidget {
           ),
         ],
       ),
-      body: PlacesList(placesList: userPlaces),
+      body: FutureBuilder(
+        future: _placesFuture,
+        builder: (context, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+            ? Center(child: CircularProgressIndicator())
+            : PlacesList(placesList: userPlaces),
+      ),
     );
   }
 }
